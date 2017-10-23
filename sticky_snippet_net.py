e80a85a24fade20910669f8885bc81d64d3a8423
sordonia120446 <samuel.ordonia@gmail.com>
2017-10-23 10:49:27 -0700

    log updates every 1000 steps

--- sticky_snippet_net.py
+++ sticky_snippet_net.py
@@ -143,7 +143,7 @@ def main(args):
     # Log the values in the "Softmax" tensor with label "probabilities"
     tensors_to_log = {"probabilities": "softmax_tensor"}
     logging_hook = tf.train.LoggingTensorHook(
-        tensors=tensors_to_log, every_n_iter=50
+        tensors=tensors_to_log, every_n_iter=1000
     )
 
     # TODO Train model

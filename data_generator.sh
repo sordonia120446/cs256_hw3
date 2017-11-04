7b2ac5b29e9bc51922dbe2a98b21435e3ed34608
sordonia120446 <samuel.ordonia@gmail.com>
2017-11-03 20:46:55 -0700

    correct from_ends arg

--- data_generator.sh
+++ data_generator.sh
@@ -15,48 +15,48 @@ mkdir test3/
 # Generate training data
 
 python sticky_snippet_generator.py 5000 0 0 ./data1/out1.txt
-python sticky_snippet_generator.py 5000 0 0 ./data1/out2.txt
-python sticky_snippet_generator.py 5000 0 0 ./data1/out3.txt
-python sticky_snippet_generator.py 5000 0 0 ./data1/out4.txt
-python sticky_snippet_generator.py 5000 0 0 ./data1/out5.txt
-python sticky_snippet_generator.py 5000 0 0 ./data1/out6.txt
+python sticky_snippet_generator.py 5000 0 1 ./data1/out2.txt
+python sticky_snippet_generator.py 5000 0 3 ./data1/out3.txt
+python sticky_snippet_generator.py 5000 0 5 ./data1/out4.txt
+python sticky_snippet_generator.py 5000 0 7 ./data1/out5.txt
+python sticky_snippet_generator.py 5000 0 20 ./data1/out6.txt
 
 python sticky_snippet_generator.py 10000 0 0 ./data2/out1.txt
-python sticky_snippet_generator.py 10000 0 0 ./data2/out2.txt
-python sticky_snippet_generator.py 10000 0 0 ./data2/out3.txt
-python sticky_snippet_generator.py 10000 0 0 ./data2/out4.txt
-python sticky_snippet_generator.py 10000 0 0 ./data2/out5.txt
-python sticky_snippet_generator.py 10000 0 0 ./data2/out6.txt
+python sticky_snippet_generator.py 10000 0 1 ./data2/out2.txt
+python sticky_snippet_generator.py 10000 0 3 ./data2/out3.txt
+python sticky_snippet_generator.py 10000 0 5 ./data2/out4.txt
+python sticky_snippet_generator.py 10000 0 7 ./data2/out5.txt
+python sticky_snippet_generator.py 10000 0 20 ./data2/out6.txt
 
 python sticky_snippet_generator.py 20000 0 0 ./data3/out1.txt
-python sticky_snippet_generator.py 20000 0 0 ./data3/out2.txt
-python sticky_snippet_generator.py 20000 0 0 ./data3/out3.txt
-python sticky_snippet_generator.py 20000 0 0 ./data3/out4.txt
-python sticky_snippet_generator.py 20000 0 0 ./data3/out5.txt
-python sticky_snippet_generator.py 20000 0 0 ./data3/out6.txt
+python sticky_snippet_generator.py 20000 0 1 ./data3/out2.txt
+python sticky_snippet_generator.py 20000 0 3 ./data3/out3.txt
+python sticky_snippet_generator.py 20000 0 5 ./data3/out4.txt
+python sticky_snippet_generator.py 20000 0 7 ./data3/out5.txt
+python sticky_snippet_generator.py 20000 0 20 ./data3/out6.txt
 
 python sticky_snippet_generator.py 60000 0 0 ./data4/out1.txt
 
 # Generate test data
 
 python sticky_snippet_generator.py 5000 0.2 0 ./test1/out1.txt
-python sticky_snippet_generator.py 5000 0.2 0 ./test1/out2.txt
-python sticky_snippet_generator.py 5000 0.2 0 ./test1/out3.txt
-python sticky_snippet_generator.py 5000 0.2 0 ./test1/out4.txt
-python sticky_snippet_generator.py 5000 0.2 0 ./test1/out5.txt
-python sticky_snippet_generator.py 5000 0.2 0 ./test1/out6.txt
+python sticky_snippet_generator.py 5000 0.2 1 ./test1/out2.txt
+python sticky_snippet_generator.py 5000 0.2 3 ./test1/out3.txt
+python sticky_snippet_generator.py 5000 0.2 5 ./test1/out4.txt
+python sticky_snippet_generator.py 5000 0.2 7 ./test1/out5.txt
+python sticky_snippet_generator.py 5000 0.2 20 ./test1/out6.txt
 
 python sticky_snippet_generator.py 5000 0.4 0 ./test2/out1.txt
-python sticky_snippet_generator.py 5000 0.4 0 ./test2/out2.txt
-python sticky_snippet_generator.py 5000 0.4 0 ./test2/out3.txt
-python sticky_snippet_generator.py 5000 0.4 0 ./test2/out4.txt
-python sticky_snippet_generator.py 5000 0.4 0 ./test2/out5.txt
-python sticky_snippet_generator.py 5000 0.4 0 ./test2/out6.txt
+python sticky_snippet_generator.py 5000 0.4 1 ./test2/out2.txt
+python sticky_snippet_generator.py 5000 0.4 3 ./test2/out3.txt
+python sticky_snippet_generator.py 5000 0.4 5 ./test2/out4.txt
+python sticky_snippet_generator.py 5000 0.4 7 ./test2/out5.txt
+python sticky_snippet_generator.py 5000 0.4 20 ./test2/out6.txt
 
 python sticky_snippet_generator.py 5000 0.6 0 ./test3/out1.txt
-python sticky_snippet_generator.py 5000 0.6 0 ./test3/out2.txt
-python sticky_snippet_generator.py 5000 0.6 0 ./test3/out3.txt
-python sticky_snippet_generator.py 5000 0.6 0 ./test3/out4.txt
-python sticky_snippet_generator.py 5000 0.6 0 ./test3/out5.txt
-python sticky_snippet_generator.py 5000 0.6 0 ./test3/out6.txt
+python sticky_snippet_generator.py 5000 0.6 1 ./test3/out2.txt
+python sticky_snippet_generator.py 5000 0.6 3 ./test3/out3.txt
+python sticky_snippet_generator.py 5000 0.6 5 ./test3/out4.txt
+python sticky_snippet_generator.py 5000 0.6 7 ./test3/out5.txt
+python sticky_snippet_generator.py 5000 0.6 20 ./test3/out6.txt
 

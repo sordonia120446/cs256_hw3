230f8558ddeee5b96a1a038c87a2bad968cc2c42
sordonia120446 <samuel.ordonia@gmail.com>
2017-11-03 20:32:14 -0700

    complete bash script

--- data_generator.sh
+++ data_generator.sh
@@ -1,17 +1,62 @@
+# Flush directories
+rm -r data*/
+rm -r test*/
+
 # Make directories
-mkdir data/
-mkdir test/
+mkdir data1/
+mkdir data2/
+mkdir data3/
+mkdir data4/
+
+mkdir test1/
+mkdir test2/
+mkdir test3/
 
 # Generate training data
 
-python sticky_snippet_generator.py 2500 0 0 ./data/out1.txt
-python sticky_snippet_generator.py 2500 0 0 ./data/out2.txt
-python sticky_snippet_generator.py 2500 0 0 ./data/out3.txt
-python sticky_snippet_generator.py 2500 0 0 ./data/out4.txt
+python sticky_snippet_generator.py 5000 0 0 ./data1/out1.txt
+python sticky_snippet_generator.py 5000 0 0 ./data1/out2.txt
+python sticky_snippet_generator.py 5000 0 0 ./data1/out3.txt
+python sticky_snippet_generator.py 5000 0 0 ./data1/out4.txt
+python sticky_snippet_generator.py 5000 0 0 ./data1/out5.txt
+python sticky_snippet_generator.py 5000 0 0 ./data1/out6.txt
+
+python sticky_snippet_generator.py 10000 0 0 ./data2/out1.txt
+python sticky_snippet_generator.py 10000 0 0 ./data2/out2.txt
+python sticky_snippet_generator.py 10000 0 0 ./data2/out3.txt
+python sticky_snippet_generator.py 10000 0 0 ./data2/out4.txt
+python sticky_snippet_generator.py 10000 0 0 ./data2/out5.txt
+python sticky_snippet_generator.py 10000 0 0 ./data2/out6.txt
+
+python sticky_snippet_generator.py 20000 0 0 ./data3/out1.txt
+python sticky_snippet_generator.py 20000 0 0 ./data3/out2.txt
+python sticky_snippet_generator.py 20000 0 0 ./data3/out3.txt
+python sticky_snippet_generator.py 20000 0 0 ./data3/out4.txt
+python sticky_snippet_generator.py 20000 0 0 ./data3/out5.txt
+python sticky_snippet_generator.py 20000 0 0 ./data3/out6.txt
+
+python sticky_snippet_generator.py 60000 0 0 ./data4/out1.txt
 
 # Generate test data
 
-python sticky_snippet_generator.py 2500 0 0 ./test/out5.txt
-python sticky_snippet_generator.py 2500 0 0 ./test/out6.txt
-python sticky_snippet_generator.py 2500 0 0 ./test/out7.txt
+python sticky_snippet_generator.py 5000 0.2 0 ./test1/out1.txt
+python sticky_snippet_generator.py 5000 0.2 0 ./test1/out2.txt
+python sticky_snippet_generator.py 5000 0.2 0 ./test1/out3.txt
+python sticky_snippet_generator.py 5000 0.2 0 ./test1/out4.txt
+python sticky_snippet_generator.py 5000 0.2 0 ./test1/out5.txt
+python sticky_snippet_generator.py 5000 0.2 0 ./test1/out6.txt
+
+python sticky_snippet_generator.py 5000 0.4 0 ./test2/out1.txt
+python sticky_snippet_generator.py 5000 0.4 0 ./test2/out2.txt
+python sticky_snippet_generator.py 5000 0.4 0 ./test2/out3.txt
+python sticky_snippet_generator.py 5000 0.4 0 ./test2/out4.txt
+python sticky_snippet_generator.py 5000 0.4 0 ./test2/out5.txt
+python sticky_snippet_generator.py 5000 0.4 0 ./test2/out6.txt
+
+python sticky_snippet_generator.py 5000 0.6 0 ./test3/out1.txt
+python sticky_snippet_generator.py 5000 0.6 0 ./test3/out2.txt
+python sticky_snippet_generator.py 5000 0.6 0 ./test3/out3.txt
+python sticky_snippet_generator.py 5000 0.6 0 ./test3/out4.txt
+python sticky_snippet_generator.py 5000 0.6 0 ./test3/out5.txt
+python sticky_snippet_generator.py 5000 0.6 0 ./test3/out6.txt
 

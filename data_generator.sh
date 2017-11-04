bebf0a9c2dceeb4bdb04af8c3f11299d0bf9d984
sordonia120446 <samuel.ordonia@gmail.com>
2017-11-03 19:53:27 -0700

    break up data gen into training & testing data sets

--- data_generator.sh
+++ data_generator.sh
@@ -1,12 +1,17 @@
+# Make directories
+mkdir data/
+mkdir test/
+
 # Generate training data
 
-python sticky_snippet_generator.py 2500 0 0 out1.txt
+python sticky_snippet_generator.py 2500 0 0 ./data/out1.txt
+python sticky_snippet_generator.py 2500 0 0 ./data/out2.txt
+python sticky_snippet_generator.py 2500 0 0 ./data/out3.txt
+python sticky_snippet_generator.py 2500 0 0 ./data/out4.txt
 
-python sticky_snippet_generator.py 2500 0 0 out2.txt
+# Generate test data
 
-python sticky_snippet_generator.py 2500 0 0 out3.txt
-
-python sticky_snippet_generator.py 2500 0 0 out4.txt
-
-python sticky_snippet_generator.py 2500 0 0 out5.txt
+python sticky_snippet_generator.py 2500 0 0 ./test/out5.txt
+python sticky_snippet_generator.py 2500 0 0 ./test/out6.txt
+python sticky_snippet_generator.py 2500 0 0 ./test/out7.txt
 

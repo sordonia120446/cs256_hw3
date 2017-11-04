31d5afe37ef2b025e0fc92c5b7dba5413d063e0d
sordonia120446 <samuel.ordonia@gmail.com>
2017-11-03 19:43:55 -0700

    add shell script

---
+++ data_generator.sh
@@ -0,0 +1,12 @@
+# Generate training data
+
+python sticky_snippet_generator.py 2500 0 0 out1.txt
+
+python sticky_snippet_generator.py 2500 0 0 out2.txt
+
+python sticky_snippet_generator.py 2500 0 0 out3.txt
+
+python sticky_snippet_generator.py 2500 0 0 out4.txt
+
+python sticky_snippet_generator.py 2500 0 0 out5.txt
+
